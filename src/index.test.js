63d4d10f217e1f9fbbd4ad5e2895cabd70ce0563
RB\binoy.sinha <binoy.sinha@redbus.com>
2018-04-13 21:47:20 +0530

    chore(releasing): Add travis config, conventional commit and semantic release
    BREAKING CHANGE: closes #1

---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names', function() {
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+
+            
+        });
+    })
+});
